4e45ded6392e860e3c082a8b57377f5e924fa618
rikyu14 <195023590+rikyu14@users.noreply.github.com>
2025-02-03 22:33:29 -0500

    Rewrite getComputerChoice function
    - Delete everything else

--- script.js
+++ script.js
@@ -1,42 +1,15 @@
 function getComputerChoice() {
-    let computerNum = Math.floor(Math.random() * 100) + 1;
+    const computerNum = Math.floor(Math.random() * 3) + 1;
 
-    if (computerNum >= 1 && computerNum <= 33) {
+    // console.log(computerNum);
+
+    if (computerNum === 1) {
         return "Rock";
-    } else if (computerNum >= 34 && computerNum <= 67) {
+    } else if (computerNum === 2) {
         return "Paper";
     } else {
         return "Scissors";
     }
 }
 
-function getHumanChoice() {
-    let humanNum = prompt("Type number your choice: Rock = 1, Paper = 2 or Scissors = 3");
-
-    if (humanNum === "1") {
-        return "Rock";
-    } else if (humanNum === "2") {
-        return "Paper";
-    } else if (humanNum === "3") {
-        return "Scissors";
-    } else {
-        return "Cannot recognize, please type again.";
-    }
-}
-
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice) {
-    if ((humanChoice === "Rock" && computerChoice === "Scissors") || (humanChoice === "Scissors" && computerChoice === "Paper") || (humanChoice === "Paper" && computerChoice === "Rock")) {
-        return "You win!";
-        humanScore += 1;
-    } else if ((computerChoice === "Rock" && humanChoice === "Scissors") || (computerChoice === "Scissors" && humanChoice === "Paper") || (computerChoice === "Paper" && humanChoice === "Rock")) {
-        return "You lose!";
-        computerScore += 1;
-    } else {
-        return "Game tie!"
-    }
-}
-
-console.log(playRound(getHumanChoice(), getComputerChoice()));
+// console.log(getComputerChoice());
